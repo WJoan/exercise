3b12a7e5289d87892b6319887fb64199f72760f7
WJoan <755168775@qq.com>
2017-03-16 22:01:18 +0800

    增加输入数字检验

--- task2-5/README.txt
+++
@@ -1,5 +0,0 @@
-JS 定时器
-http://javascript.ruanyifeng.com/advanced/timer.html
-
-DOM基本操作
-《JavaScript DOM 编程艺术》
--- task2-5/js/task5.js
+++ task2-5/js/task5.js
@@ -38,6 +38,10 @@
  	inputNode.value = "";
  	inputNode.focus();
  	// 检查用户输入数字合法性
+ 	if(!Number(num)) {
+ 		alert("请输入符合要求的数字");
+ 		return false;
+ 	}
  	if(Number(num) < 10 || Number(num) > 100){
  		alert("请输入符合要求的数字");
  		return false;
