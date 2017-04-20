4a7ffe95890489fdd6bdca83dc61d741fb5d760d
WJoan <755168775@qq.com>
2017-04-20 17:17:41 +0800

    update

---
+++ yaoyao/form/form-check-task.js
@@ -0,0 +1,18 @@
+let input = document.getElementById("user-name");
+let rule = document.getElementById("rule");
+let right = document.getElementById("right");
+let error = document.getElementById("error");
+
+let inputText = input.value.split('');
+
+function showTips(style){
+	input.class = style;
+}
+
+if(inputText.length == 0){
+
+}
+
+for( let i = 0; i < inputText.length; i++){
+	if()
+}
