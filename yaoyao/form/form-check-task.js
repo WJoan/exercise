3c578e976707b3ecb3732121dd58866698d36570
WJoan <755168775@qq.com>
2017-04-26 09:29:55 +0800

    update

--- yaoyao/form/form-check-task.js
+++ yaoyao/form/form-check-task.js
@@ -5,14 +5,36 @@ let error = document.getElementById("error");
 
 let inputText = input.value.split('');
 
-function showTips(style){
-	input.class = style;
+// 显示提示信息
+let showTips = function (style) {
+	input.class = 'input-'+style;
+	let node = document.getElementById(style);
+	let childrens = node.parentNode.children;
+	for(let i = 0; i < childrens.length; i++){
+		childrens[i].style.display='none';
+	}
+	node.style.display = 'block';
 }
 
-if(inputText.length == 0){
+// 显示错误或者正确提示
+let checkRule = function(){
+	let re = /[\w|\u4e00-\u9fa5]{4,16}/g;
+	let value = this.value;
+	if(!value){
+		showTips('rule');
+	}
+	else if(re.test(value)){
+		showTips('right');
+	}
+	else{
+		showTips('error');
+	}
 
 }
 
-for( let i = 0; i < inputText.length; i++){
-	if()
+// 绑定
+let init = function(){
+	input.addEventListener('blur', checkRule);
 }
+
+init();
