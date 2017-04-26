9dd1db1d5eb89470ad6614ef72ba84c4d5684ec1
WJoan <755168775@qq.com>
2017-04-26 11:17:54 +0800

    update

--- yaoyao/form/form-check-task.js
+++ yaoyao/form/form-check-task.js
@@ -1,40 +1,57 @@
 let input = document.getElementById("user-name");
-let rule = document.getElementById("rule");
-let right = document.getElementById("right");
-let error = document.getElementById("error");
+let checkBtn = document.querySelector(".checkBtn");
 
 let inputText = input.value.split('');
 
 // 显示提示信息
 let showTips = function (style) {
-	input.class = 'input-'+style;
+	input.className = style;
 	let node = document.getElementById(style);
 	let childrens = node.parentNode.children;
 	for(let i = 0; i < childrens.length; i++){
 		childrens[i].style.display='none';
 	}
 	node.style.display = 'block';
+	return false;
 }
 
 // 显示错误或者正确提示
 let checkRule = function(){
-	let re = /[\w|\u4e00-\u9fa5]{4,16}/g;
-	let value = this.value;
-	if(!value){
-		showTips('rule');
+	let re = /[\w|\u4e00-\u9fa5]+/g;
+	let value = input.value;
+	
+	function len(str){
+		var len = 0;
+		for(var i=0; i<str.length; i++){
+			var val = str.charCodeAt(i);
+			if(val > 0 && val < 128){
+				len++;
+			}
+			else{
+				len = len + 2;
+			}
+		}
+		return len;
 	}
-	else if(re.test(value)){
+	
+	let l = len(value);
+
+	if(value == ''){
+		showTips('error-null');
+	}
+	else if(re.test(value) && l >3 && l < 17){
 		showTips('right');
 	}
 	else{
-		showTips('error');
+		showTips('error-len');
 	}
-
+	return false;
 }
 
 // 绑定
 let init = function(){
-	input.addEventListener('blur', checkRule);
+	showTips('rule');
+	checkBtn.addEventListener('click', checkRule);
 }
 
 init();
