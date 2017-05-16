debbd06454de1a3cac5d3b3c50cede1212665a6d
WJoan <755168775@qq.com>
2017-05-16 15:11:32 +0800

    update

--- test/memory_test.js
+++ test/memory_test.js
@@ -2,36 +2,47 @@ var body = document.getElementsByTagName('body')[0];
 var addDom = document.getElementById("addBtn");
 var deleDom = document.getElementById("deleBtn");
 var deleDom2 = document.getElementById("deleBtn2");
+var deleDom3 = document.getElementById("deleBtn3");
+var content = document.getElementById("content");
+
+
 
 addDom.addEventListener('click', function(){
-	var text = document.createTextNode('p node');
-	var str = ' new';
-	var f = function(){
-			var text = document.createTextNode(str);
-			this.appendChild(text);
-		};
-	for(var i = 0; i < 10000; i++){
+	var data = [1,2,4];
+
+	function f(){
+		console.log(data[0]);
+	};
+	 for(var i = 0; i < 100000; i++){
+		var text = document.createTextNode('p');
 		var pNode = document.createElement('p');
-		var text = document.createTextNode('p node');
 		pNode.appendChild(text);
 		pNode.addEventListener('click', f);
-		body.appendChild(pNode);
-	}
+		content.appendChild(pNode);	
+		
+	 }
+
+	
 });
 
 deleDom.addEventListener('click', function(){
-	var list = document.getElementsByTagName('p');
-	while(list[0]){
-		body.removeChild(list[0]);
-	}
-	
+	content.innerHTML = '';
 });
+
 
 deleDom2.addEventListener('click', function(){
 	var list = document.getElementsByTagName('p');
 	while(list[0]){
 		list[0].onclick = null;
-		body.removeChild(list[0]);
+		delete content.removeChild(list[0]);
 	}
-	
+
 });
+
+deleDom3.addEventListener('click', function(){
+	$('p').each(function(){
+		this.remove();
+	})
+});
+
+
