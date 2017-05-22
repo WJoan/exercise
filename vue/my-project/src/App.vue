eae08c304562f1522879649889edd5be3101c19f
WJoan <755168775@qq.com>
2017-05-22 16:15:22 +0800

    todo list

--- vue/my-project/src/App.vue
+++ vue/my-project/src/App.vue
@@ -1,13 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1>{{ title }}</h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">
+        {{ item.label }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data: function() {
+    return {
+      title: 'Todo list',
+      items: [
+        {
+          label: 'codeing',
+          isFinished: false
+        },
+        {
+          label: 'walking',
+          isFinished: true
+        }
+      ],
+      newItem: '',
+    }
+  },
+  methods: {
+    toggleFinish: function(item){
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function () {
+      this.items.push({
+        label: this.newItem,
+        isFinished: false
+      });
+      this.newItem = '';
+    }
+  }
 }
 </script>
 
@@ -18,6 +50,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  width: 300px;
+}
+.finished{
+  color: green;
+  font-weight: 800;
 }
 </style>
