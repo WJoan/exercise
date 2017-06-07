acc552f2ba8065eed469b431f5da6aa4dc9ecc3a
WJoan <755168775@qq.com>
2017-06-07 20:42:57 +0800

    add annotations & check the code specification

--- baidu_ife/business/carousel.js
+++ baidu_ife/business/carousel.js
@@ -4,11 +4,18 @@ let btns = document.getElementById("btnList").getElementsByTagName("span");
 let prev = document.getElementById("arrowLeft");
 let next = document.getElementById("arrowRight");
 
+// 当前图片索引
 let index = 0;
+// 图片是否正在移动
 let animated = false;
+// 定时器
 let timer;
 
-let moveImg = function (offset) {
+/**
+ * 移动图片
+ * @param  {number} offset 移动像素
+ */
+function moveImg(offset) {
 	let getLeft = () => parseInt(imgList.style.left);
 	let newLeft = getLeft() + offset;
 	let time = 500;	// 位移总时间
@@ -35,7 +42,10 @@ let moveImg = function (offset) {
 	//imgList.style.left = parseInt(imgList.style.left) + offset + 'px';
 }
 
-let showButton = function () {
+/**
+ * 高亮当前图片检索圆点
+ */
+function showButton() {
 	for (var i = 0; i < btns.length; i++) {
 		if(btns[i].className == 'on'){
 			btns[i].className = '';
@@ -45,15 +55,21 @@ let showButton = function () {
 	btns[index].className = 'on';
 }
 
-let autoMove = function () {
+/**
+ * 自动轮播图片
+ */
+function autoMove() {
 	timer = setInterval( () => next.click(), 3000 );
 }
 
-let stopMove = function () {
+/**
+ * 停止轮播
+ */
+function stopMove() {
 	clearInterval(timer);
 }
 
-// 下一张
+// 点击切换下一张
 next.addEventListener('click', () => {
 	if( index == 4){		
 		index = 0;		
@@ -68,7 +84,7 @@ next.addEventListener('click', () => {
 	moveImg(-1024);
 });
 
-// 上一张
+// 点击切换上一张
 prev.addEventListener('click', () => {
 	if( index == 0 ) {
 		index = 4;
@@ -83,19 +99,24 @@ prev.addEventListener('click', () => {
 	moveImg(1024);
 });
 
-
+// 为圆点绑定点击事件
 for (var i = 0; i < btns.length; i++) {
-	btns[i].addEventListener('click', function(){
+	// 点击切换图片
+	btns[i].addEventListener('click', () => {
 		let desIndex = parseInt( this.getAttribute("data-index") );
 
+		// 判断当前图片是否为要切换的图片
+		// 如果是，则不执行任何动画效果
 		if( desIndex == index ) return false;
-		
+		// 否则切换图片
 		moveImg(-1024 * ( desIndex - index ) );
-
 		index = desIndex;
 		showButton();
 	});
 }
 
+// 鼠标移动到图片上停止自动播放
 container.addEventListener('mouseover', stopMove );
+
+// 鼠标移出图片开始自动播放
 container.addEventListener('mouseout', autoMove );
